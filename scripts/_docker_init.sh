f6e1fd4fbfd8ad2e0081134706725652772a54d6
Shuntaro Sato <shuntaro.st@gmail.com>
2022-04-28 05:43:04 +0000

    Fix scripts/_docker_init.sh

--- scripts/_docker_init.sh
+++ scripts/_docker_init.sh
@@ -9,7 +9,6 @@ groupmod -g "$HOST_GID" developer
     echo "export PYTHONPATH=${PYTHONPATH}"
     echo "export PYTHONIOENCODING=utf-8"
     echo "export TVM_LIBRARY_PATH=${TVM_LIBRARY_PATH}"
-    # echo "export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}"
     echo "export PATH=${PATH}"
     echo "cd /workspaces/arachne-runtime"
 } >> /home/developer/.bashrc
