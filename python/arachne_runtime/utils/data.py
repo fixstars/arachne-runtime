90c4e275b6316aef1c6d4684938016a89b9ba124
Takafumi Kubota <takafumi.kubota@fixstars.com>
2022-05-09 15:00:50 +0900

    update .gitignore and remove unused files

--- python/arachne_runtime/utils/data.py
+++ python/arachne_runtime/utils/data.py
@@ -1,59 +1,59 @@
-from dataclasses import dataclass
-from enum import Enum
-from typing import List, Optional
+# from dataclasses import dataclass
+# from enum import Enum
+# from typing import List, Optional
 
 
-@dataclass
-class TensorSpec:
-    """This class contains the tensor information.
+# @dataclass
+# class TensorSpec:
+#     """This class contains the tensor information.
 
-    Attributes:
-        name (str): tensor name.
-        shape (List[int]): tensor shape.
-        dtype (str): tensor data type.
-    """
+#     Attributes:
+#         name (str): tensor name.
+#         shape (List[int]): tensor shape.
+#         dtype (str): tensor data type.
+#     """
 
-    name: str
-    shape: List[int]
-    dtype: str
-
-
-@dataclass
-class ModelSpec:
-    """This class keeps the input and output tensor information of the model.
-
-    Attributes:
-        inputs (List[arachne.data.TensorSpec]): input tensors
-        outputs (List[arachne.data.TensorSpec]): output tensors
-    """
-
-    inputs: List[TensorSpec]
-    outputs: List[TensorSpec]
+#     name: str
+#     shape: List[int]
+#     dtype: str
 
 
-class ModelFormat(Enum):
-    """This contains DNN model formats supported in arachne."""
+# @dataclass
+# class ModelSpec:
+#     """This class keeps the input and output tensor information of the model.
 
-    TVM = 0
-    TF_PB = 1
-    KERAS_H5 = 2
-    TF_SAVED_MODEL = 3
-    TFLITE = 4
-    PYTORCH = 5
-    ONNX = 6
-    OPENVINO = 7
-    CAFFE = 8
+#     Attributes:
+#         inputs (List[arachne.data.TensorSpec]): input tensors
+#         outputs (List[arachne.data.TensorSpec]): output tensors
+#     """
+
+#     inputs: List[TensorSpec]
+#     outputs: List[TensorSpec]
 
 
-@dataclass
-class Model:
-    """This represents DNN models in arachne.
+# class ModelFormat(Enum):
+#     """This contains DNN model formats supported in arachne."""
 
-    Attributes:
-        path (str): The path to model file or directory.
-        spec (arachne.data.ModelSpec, optional): the tensor specification  for this model.
-    """
+#     TVM = 0
+#     TF_PB = 1
+#     KERAS_H5 = 2
+#     TF_SAVED_MODEL = 3
+#     TFLITE = 4
+#     PYTORCH = 5
+#     ONNX = 6
+#     OPENVINO = 7
+#     CAFFE = 8
 
-    path: str
-    format: ModelFormat
-    spec: Optional[ModelSpec] = None
+
+# @dataclass
+# class Model:
+#     """This represents DNN models in arachne.
+
+#     Attributes:
+#         path (str): The path to model file or directory.
+#         spec (arachne.data.ModelSpec, optional): the tensor specification  for this model.
+#     """
+
+#     path: str
+#     format: ModelFormat
+#     spec: Optional[ModelSpec] = None
