7379c02bab84c86c6446c52c5285eaef497b3c85
Shuntaro Sato <shuntaro.st@gmail.com>
2023-07-31 16:33:08 +0900

    Update dependencies

--- docker/install/install_poetry.sh
+++ docker/install/install_poetry.sh
@@ -4,4 +4,4 @@ set -e
 set -u
 set -o pipefail
 
-curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/install-poetry.py | POETRY_HOME=/usr/local/poetry python3 - --version 1.2.0a2
+curl -sSL https://install.python-poetry.org | POETRY_HOME=/usr/local/poetry python3 - --version 1.5.1
